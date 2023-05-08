852000d29c5a5519d28b984a481729493671b0bb
KMalif <khafidulmualif2019@gmail.com>
2023-05-08 22:17:20 +0700

    [Alif][Add] initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className='bg-slate-900 grid place-content-center min-h-screen'>
+            <div className='flex gap-2'></div>
+        </div>
+    )
+}
+
+export default App
